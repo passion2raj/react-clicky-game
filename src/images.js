0eca8d03fe28d1c8379257050a528e1fdcbcc632
Rajesh Rana <asra8175@gmail.com>
2019-11-13 18:50:45 -0500

    App Deploy

---
+++ src/images.js
@@ -0,0 +1,29 @@
+import Avenger1 from "./images/Avenger1.png";
+import Avenger2 from "./images/Avenger2.png";
+import Avenger3 from "./images/Avenger3.png";
+import Avenger4 from "./images/Avenger4.png";
+import Avenger5 from "./images/Avenger5.png";
+import Avenger6 from "./images/Avenger6.png";
+import Avenger7 from "./images/Avenger7.png";
+import Avenger8 from "./images/Avenger8.png";
+import Avenger9 from "./images/Avenger9.png";
+import Avenger10 from "./images/Avenger10.png";
+import Avenger11 from "./images/Avenger11.png";
+import Avenger12 from "./images/Avenger12.png";
+
+const images = [
+  Avenger1,
+  Avenger2,
+  Avenger3,
+  Avenger4,
+  Avenger5,
+  Avenger6,
+  Avenger7,
+  Avenger8,
+  Avenger9,
+  Avenger10,
+  Avenger11,
+  Avenger12
+];
+
+export default images;
