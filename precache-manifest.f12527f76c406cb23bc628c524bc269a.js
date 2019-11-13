156861f587335419a0f3e219450dec4be6d463b2
Rajesh Rana <asra8175@gmail.com>
2019-11-13 18:56:10 -0500

    Updates

---
+++ precache-manifest.f12527f76c406cb23bc628c524bc269a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f387cf0eb30b11fbd4eace2ac69cf637",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "081e5cf6486c6c3bdf40",
+    "url": "/react-clicky-game/static/css/main.5146cfc7.chunk.css"
+  },
+  {
+    "revision": "06e6ee25fb0bcdfe2f0e",
+    "url": "/react-clicky-game/static/js/2.7b82f4fe.chunk.js"
+  },
+  {
+    "revision": "081e5cf6486c6c3bdf40",
+    "url": "/react-clicky-game/static/js/main.2d4a43dd.chunk.js"
+  },
+  {
+    "revision": "467520b62b592fe275be",
+    "url": "/react-clicky-game/static/js/runtime-main.53725655.js"
+  },
+  {
+    "revision": "b18165ef17fbedcb8a586781ec61a858",
+    "url": "/react-clicky-game/static/media/Avenger6.b18165ef.png"
+  }
+]);
